2ba1cf38c9ef9e36ea9bd72d874d151d13b9b059
Keagan Edward Wangsakentjana <keagedw.official@gmail.com>
2026-01-23 13:58:23 +0800

    Renamed Duke to Nikolaus Implemented greeting and farewell

---
+++ src/main/java/Nikolaus.java
@@ -0,0 +1,16 @@
+public class Nikolaus {
+    public static void main(String[] args) {
+        greet();
+        System.out.println();
+        bidFarewell();
+    }
+
+    public static void greet() {
+        System.out.println("Greetings!!! I'm Nikolaus, your personal chatbot!!!");
+        System.out.println("What can I do for you today???");
+    }
+
+    public static void bidFarewell() {
+        System.out.println("Goodbye!!! Hope to see you again soon!!!");
+    }
+}
