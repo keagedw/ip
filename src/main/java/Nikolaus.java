5f799d1e444d84ea701deca94cd05b56eeef7b8a
Keagan Edward Wangsakentjana <keagedw.official@gmail.com>
2026-01-27 00:46:47 +0800

    Added logo

--- src/main/java/Nikolaus.java
+++ src/main/java/Nikolaus.java
@@ -6,6 +6,22 @@ public class Nikolaus {
     }
 
     public static void greet() {
+        String logo = "       ::::    :::   :::::::::::   :::    :::    ::::::::     :::              :::        :::    :::     :::::::: \n" +
+            "      :+:+:   :+:       :+:       :+:   :+:    :+:    :+:    :+:            :+: :+:      :+:    :+:    :+:    :+: \n" +
+            "     :+:+:+  +:+       +:+       +:+  +:+     +:+    +:+    +:+           +:+   +:+     +:+    +:+    +:+         \n" +
+            "    +#+ +:+ +#+       +#+       +#++:++      +#+    +:+    +#+          +#++:++#++:    +#+    +:+    +#++:++#++   \n" +
+            "   +#+  +#+#+#       +#+       +#+  +#+     +#+    +#+    +#+          +#+     +#+    +#+    +#+           +#+    \n" +
+            "  #+#   #+#+#       #+#       #+#   #+#    #+#    #+#    #+#          #+#     #+#    #+#    #+#    #+#    #+#     \n" +
+            " ###    ####   ###########   ###    ###    ########     ##########   ###     ###     ########      ########       ";
+        for (int i = 0; i < 114; i++) {
+            System.out.print('\u2500');
+        }
+        System.out.println();
+        System.out.println(logo);
+        for (int i = 0; i < 114; i++) {
+            System.out.print('\u2500');
+        }
+        System.out.println();
         System.out.println("Greetings!!! I'm Nikolaus, your personal chatbot!!!");
         System.out.println("What can I do for you today???");
     }
@@ -14,3 +30,5 @@ public class Nikolaus {
         System.out.println("Goodbye!!! Hope to see you again soon!!!");
     }
 }
+
+
